68c35f93cdaf2e7400acf10ec56e71103dd1ff62
Ser-Pol <semnnae@gmail.com>
2018-11-15 15:04:50 +0300

    1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0;

--- lesson2/src/geekbrains/Main.java
+++ lesson2/src/geekbrains/Main.java
@@ -1,8 +1,26 @@
 package geekbrains;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
 	// write your code here
+        revertArray();
+    }
+
+    public static void revertArray()
+    {
+        byte[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+
+        System.out.println("Original array:");
+        System.out.println(Arrays.toString(arr));
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = (byte)(1 - arr[i]);
+
+        System.out.println("Changed array:");
+        System.out.println(Arrays.toString(arr));
     }
 }
