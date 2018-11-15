8f47257ac53b80888cbb83dff7d49f8ebdf07012
Ser-Pol <s.a.polynskiy@gmail.com>
2018-11-16 00:47:39 +0300

    - Initialization of board - Method for print of board

--- cross-zero/src/geekbrains/Main.java
+++ cross-zero/src/geekbrains/Main.java
@@ -1,8 +1,45 @@
 package geekbrains;
 
-public class Main {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
+public class Main
+{
+    public static int SIZE;
+    public static char[][] board;
+    public static char EMPTY = '*';
+
+    public static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
 	// write your code here
+        System.out.println("Enter size of the board...");
+        SIZE = in.nextInt();
+        board = new char[SIZE][SIZE];
+        initBoard();
+        printBoard();
+
+    }
+
+    public static void initBoard()
+    {
+        for (int i = 0; i < SIZE; i++)
+            for (int j = 0; j < SIZE; j++)
+                board[i][j] = EMPTY;
+    }
+
+    public static void printBoard()
+    {
+        System.out.printf("%12d", 0);
+        for (int i = 1; i < SIZE; i++) System.out.printf("%5d", i);
+        System.out.println();
+        System.out.println();
+        for (int i = 0; i < SIZE; i++)
+        {
+            System.out.printf("%7d", i);
+            for (int j = 0; j < SIZE; j++)
+                System.out.printf("%5c", board[i][j]);
+            System.out.println();
+        }
     }
 }
