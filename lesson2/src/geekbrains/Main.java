9a45cd15865ae6dfa400b274ab127994bf3ba7bd
Ser-Pol <semnnae@gmail.com>
2018-11-15 15:23:07 +0300

    3. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2;

--- lesson2/src/geekbrains/Main.java
+++ lesson2/src/geekbrains/Main.java
@@ -13,6 +13,10 @@ public class Main {
         System.out.println();
 
         fillArray();
+
+        System.out.println();
+
+        changeArray();
     }
 
     public static void revertArray()
@@ -34,6 +38,16 @@ public class Main {
 
         for (int i = 0, k = 0; k < arr.length; i += 3, k++)
             arr[k] = i;
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void changeArray()
+    {
+        int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+
+        for (int i = 0; i < arr.length; i++)
+            if (arr[i] < 6) arr[i] *= 2;
 
         System.out.println(Arrays.toString(arr));
     }
